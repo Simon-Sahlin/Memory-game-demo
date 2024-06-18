a43d0a5b8db057bbc10005a82536b63246367991
Simon-Sahlin <simonsahlin@somin.se>
2024-06-18 17:15:20 +0200

    Shuffle cards on click

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,29 @@ function App() {
       const response = await fetchImages();
       setImages(response.data)
     }
+    console.log("huh")
     fetchData();
   }, []);
 
-  const bruh = true;
+  function handleClick(index){
+    console.log("Clicked " + index)
+    let newArray = [...imagesArray];
+    newArray = newArray.sort(() => Math.random() - 0.5);;
+    setImages(newArray);
+  }
 
   return (
     <>
       <h1>Welcome to Cat Memory</h1>
       <h2>Get points by clicking on an image but don't click on any more than once!</h2>
-      {console.log(imagesArray)}
+      {/* {console.log("NEW RENDER!! " + (imagesArray.length > 0 ? imagesArray[0].title : "(Empty)"))} */}
+      {/* {console.log(imagesArray)} */}
+
       <div className="cardsWrapper">
         {imagesArray.length <= 0 ? <p>Loading Images...</p> : 
-          imagesArray.map((item) =>{
+          imagesArray.map((item, index) =>{
             return (
-              <div key={item.id} className="card">
+              <div key={item.id} className="card" onClick={()=>handleClick(index)}>
                 <p>{item.title}</p>
                 <img src={item.images.original.url}/>
               </div>
